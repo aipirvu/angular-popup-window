e429b23be1588ba3f9ab2043c4104c04e7b25dff
Alexandru Pirvu <aipirvu@gmail.com>
2022-02-11 18:36:46 +0200

    dragging window done.

--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-window',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./window.component.css']
 })
 export class WindowComponent implements OnInit {
+  isHeaderSelected = false;
 
-  constructor() { }
+  constructor(private elRef: ElementRef) {
+  }
 
   ngOnInit(): void {
   }
 
+  startMoving(event: any): void {
+    const that = this;
+    const hostElement = this.elRef.nativeElement;
+    const windowOriginalOffsetTop = hostElement.offsetTop;
+    const windowOriginalOffsetLeft = hostElement.offsetLeft;
+    const mouseOriginalPageX = event.pageX;
+    const mouseOriginalPageY = event.pageY;
+    that.isHeaderSelected = true;
+
+    const mousemoveEventListener = (mouseEvent: MouseEvent) => {
+      this.elRef.nativeElement.style.left = (windowOriginalOffsetLeft - mouseOriginalPageX + mouseEvent.pageX) + "px";
+      this.elRef.nativeElement.style.top = (windowOriginalOffsetTop - mouseOriginalPageY + mouseEvent.pageY) + "px";
+      console.log({x: hostElement.style.left, y: hostElement.style.top});
+    }
+
+    const mouseupEventListener = () => {
+      document.removeEventListener("mousemove", mousemoveEventListener);
+      document.removeEventListener("mouseup", mouseupEventListener);
+      that.isHeaderSelected = false;
+    }
+
+    document.addEventListener("mousemove", mousemoveEventListener);
+    document.addEventListener("mouseup", mouseupEventListener);
+  }
 }
